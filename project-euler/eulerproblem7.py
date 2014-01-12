c69faae28c76e57b8fba85e070193088443edee0
Red-Tide <alfredswinton@gmail.com>
2014-01-12 07:19:46 -0500

    Added some comments to eulerproblem7 deleted problem9

--- project-euler/eulerproblem7.py
+++ project-euler/eulerproblem7.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+''' The below code tests for primacy, and solves problems 7 on project euler.It does so by taking a
+    number a dividing it by all primes up until its square root. If any of the
+    former primes divide equally into the the number it is not prime and is
+    rejected as false; however if it will not be divided by previous primes it
+    is prime and is added to the list of primes to divide into future numbers.
+    The methodology is called the sieve of Eratosthenes:
+    http://www.wikipedia.com/sieve_of_eratosthenes'''
+
 def primeTest(testNum,primes):
     for prime in primes:
         if prime > (testNum**0.5):
